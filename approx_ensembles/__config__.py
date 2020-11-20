635a77c4ca1281a202dd853bca3ca089368ca915
Efthymios Tzinis <etzinis2@illinois.edu>
2020-11-20 01:45:15 -0600

    Adding paths accessible from config

---
+++ approx_ensembles/__config__.py
@@ -0,0 +1,10 @@
+from os.path import expanduser
+import os
+# Save directory paths
+home_folder = expanduser("~")
+other_tree_path = ''
+MNIST_LOG_PATH = os.path.join(home_folder, other_tree_path,
+                              'approx_ensembles_log', 'mnist')
+
+# Datasets paths
+MNIST_PATH='/mnt/data/mnist'
