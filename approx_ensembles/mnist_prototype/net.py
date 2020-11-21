4af0b7018b05cbb5b1b8fc71a253d176abc7f939
Efthymios Tzinis <etzinis2@illinois.edu>
2020-11-20 19:33:54 -0600

    network cleaning code for unused parameters

--- approx_ensembles/mnist_prototype/net.py
+++ approx_ensembles/mnist_prototype/net.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class MNISTnet(nn.Module):
-    def __init__(self, params):
+    def __init__(self):
         super(MNISTnet, self).__init__()
         self.convs = nn.Sequential(nn.Conv2d(1, 32, kernel_size=5, padding=2),
                                    nn.ReLU(),
@@ -17,7 +17,6 @@ class MNISTnet(nn.Module):
                                    nn.Conv2d(32, 32, kernel_size=5, padding=2),
                                    nn.ReLU(),
                                    nn.Conv2d(32, 1, kernel_size=5, padding=2))
-
 
     def forward(self, x):
         return self.convs(x)
