1a855164cc0a93c1c0084fc3511a52914ad1622f
Efthymios Tzinis <etzinis2@illinois.edu>
2020-11-25 20:09:46 -0600

    Parameterize the network for the number of intermediate layers

--- approx_ensembles/mnist_prototype/net.py
+++ approx_ensembles/mnist_prototype/net.py
@@ -1,22 +1,18 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class MNISTnet(nn.Module):
-    def __init__(self):
+    def __init__(self, n_intermediate_layers=4):
         super(MNISTnet, self).__init__()
-        self.convs = nn.Sequential(nn.Conv2d(1, 32, kernel_size=5, padding=2),
-                                   nn.ReLU(),
-                                   nn.Conv2d(32, 32, kernel_size=5, padding=2),
-                                   nn.ReLU(),
-                                   nn.Conv2d(32, 32, kernel_size=5, padding=2),
-                                   nn.ReLU(),
-                                   nn.Conv2d(32, 32, kernel_size=5, padding=2),
-                                   nn.ReLU(),
-                                   nn.Conv2d(32, 32, kernel_size=5, padding=2),
-                                   nn.ReLU(),
-                                   nn.Conv2d(32, 1, kernel_size=5, padding=2))
+        layers_list = [nn.Conv2d(1, 32, kernel_size=5, padding=2), nn.ReLU()] +\
+            [nn.Conv2d(32, 32, kernel_size=5, padding=2),
+             nn.ReLU()] * n_intermediate_layers + \
+            [nn.Conv2d(32, 1, kernel_size=5, padding=2)]
+        self.convs = nn.Sequential(*layers_list)
 
     def forward(self, x):
         return self.convs(x)
+
+if __name__ == "__main__":
+    model = MNISTnet(n_intermediate_layers=0)
+    print(model)
