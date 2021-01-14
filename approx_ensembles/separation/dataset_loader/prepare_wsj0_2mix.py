372def6bf522f3d7109b53e049a4352d99c46cbb
Efthymios Tzinis <etzinis2@illinois.edu>
2021-01-13 18:55:17 -0600

    Real time parsing of wham dataset for mixture combinations

--- approx_ensembles/separation/dataset_loader/prepare_wsj0_2mix.py
+++
@@ -1,7 +0,0 @@
-"""!
-@brief Prepare WHAM dataset in speaker format in order to be able to get
-proper splits as requested.
-@author Efthymios Tzinis {etzinis2@illinois.edu}
-@copyright University of Illinois at Urbana-Champaign
-"""
-
