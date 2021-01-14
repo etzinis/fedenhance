d9c239e99471655dfee01183bba578683cd7f9d3
Efthymios Tzinis <etzinis2@illinois.edu>
2021-01-13 18:54:30 -0600

    Abstract datset more static type checks for dictionary and inhgeritance

---
+++ approx_ensembles/separation/dataset_loader/prepare_wsj0_2mix.py
@@ -0,0 +1,7 @@
+"""!
+@brief Prepare WHAM dataset in speaker format in order to be able to get
+proper splits as requested.
+@author Efthymios Tzinis {etzinis2@illinois.edu}
+@copyright University of Illinois at Urbana-Champaign
+"""
+
