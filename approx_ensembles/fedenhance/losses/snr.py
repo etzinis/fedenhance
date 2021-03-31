def1015ff9e8d34374d561aa00a2fff1c8752ec7
Efthymios Tzinis <etzinis2@illinois.edu>
2021-03-30 22:52:48 -0500

    SNR supervised loss for fixed 2 sources and 2 noises

--- approx_ensembles/fedenhance/losses/snr.py
+++ approx_ensembles/fedenhance/losses/snr.py
@@ -10,15 +10,17 @@ import torch.nn as nn
 import itertools
 from torch.nn.modules.loss import _Loss
 
-class PermInvariantSNRwithZeroRefs(nn.Module):
+
+class FixedMixIT2Sources2NoisesSNRwithZeroRefs(nn.Module):
     """!
-    Class for SNR computation between reconstructed signals and
-    target wavs with compensation for zero reference signals."""
+    Class for SNR computation between 4 reconstructed signals and
+    2 reference speakers and 2 reference noises."""
 
     def __init__(self,
                  zero_mean=False,
-                 n_sources=None,
                  backward_loss=True,
+                 supervised=True,
+                 sources_weight=0.5,
                  inactivity_threshold=-40.,
                  return_individual_results=False):
         """
@@ -28,8 +30,173 @@ class PermInvariantSNRwithZeroRefs(nn.Module):
         super().__init__()
         self.perform_zero_mean = zero_mean
         self.backward_loss = backward_loss
+        self.supervised = supervised
+        self.sources_weight=sources_weight
         self.permutations = list(itertools.permutations(
-            torch.arange(n_sources)))
+            torch.arange(2)))
+        self.permutations_tensor = torch.LongTensor(self.permutations)
+        self.inactivity_threshold = inactivity_threshold
+        self.return_individual_results = return_individual_results
+
+    def normalize_input(self, signals):
+        if self.perform_zero_mean:
+            return signals - torch.mean(signals, dim=-1, keepdim=True)
+        return signals
+
+    @staticmethod
+    def dot(x, y):
+        return torch.sum(x * y, dim=-1, keepdim=True)
+
+    def compute_permuted_snrs_and_inactive(self,
+                                           pr_batch,
+                                           t_batch,
+                                           activity_mask,
+                                           denom_stabilizer,
+                                           eps=1e-9):
+        # Compute SNR for the active and inactive sources
+        nom = self.dot(t_batch, t_batch) + eps
+        error = pr_batch - t_batch
+        denom = self.dot(error, error) + denom_stabilizer + eps
+        return 10. * activity_mask * torch.log10(nom / denom + eps)
+
+    def compute_permuted_snrs(self,
+                              permuted_pr_batch,
+                              t_batch,
+                              eps=1e-9):
+        s_t = t_batch
+        e_t = permuted_pr_batch - s_t
+        snrs = 10 * torch.log10(self.dot(s_t, s_t) /
+                                (self.dot(e_t, e_t) + eps))
+        return snrs
+
+    def compute_snr(self,
+                    pr_batch,
+                    ref_sources_batch,
+                    ref_noises_batch,
+                    initial_mixture,
+                    eps=1e-9,
+                    thresh=0.001):
+
+        mixture_power = self.dot(initial_mixture, initial_mixture)
+        mixture_power_repeated = mixture_power.repeat(1, 2, 1)
+
+        # Compute the maximum SNR obtained by the combinations of estimated
+        # sources {0, 1} -> reference sources {0, 1} and estimated sources
+        # estimated sources {2, 3} -> reference noises {0, 1}.
+        if self.supervised:
+            ref_sources_powers = self.dot(ref_sources_batch, ref_sources_batch)
+            ref_noises_powers = self.dot(ref_noises_batch, ref_noises_batch)
+            sources_input_snr = 10. * torch.log10(
+                ref_sources_powers / (mixture_power + eps))
+            noises_input_snr = 10. * torch.log10(
+                ref_noises_powers / (mixture_power + eps))
+            sources_activity_mask = sources_input_snr.ge(
+                self.inactivity_threshold)
+            noises_activity_mask = noises_input_snr.ge(
+                self.inactivity_threshold)
+
+            sources_active_stabilizer = sources_activity_mask * ref_sources_powers
+            sources_inactive_stabilizer = (
+                                              ~sources_activity_mask) * mixture_power_repeated
+            sources_denom_stabilizer = thresh * (
+                        sources_active_stabilizer + sources_inactive_stabilizer)
+
+            noises_active_stabilizer = noises_activity_mask * ref_noises_powers
+            noises_inactive_stabilizer = (
+                                             ~noises_activity_mask) * mixture_power_repeated
+            noises_denom_stabilizer = thresh * (
+                        noises_active_stabilizer + noises_inactive_stabilizer)
+
+            num_active_sources = sources_activity_mask.sum([-2, -1]).unsqueeze(-1)
+            num_active_noises = noises_activity_mask.sum([-2, -1]).unsqueeze(-1)
+
+            sources_snr_l = []
+            for perm in self.permutations:
+                permuted_pr_batch = pr_batch[:, :2][:, perm]
+                snr = self.compute_permuted_snrs_and_inactive(
+                    permuted_pr_batch, ref_sources_batch, sources_activity_mask,
+                    sources_denom_stabilizer, eps=eps)
+                sources_snr_l.append(snr)
+            all_sources_snrs = torch.cat(sources_snr_l, -1)
+            best_sources_snr, _ = torch.max(all_sources_snrs.sum(-2) * num_active_sources, -1)
+
+            noises_snr_l = []
+            for perm in self.permutations:
+                permuted_pr_batch = pr_batch[:, 2:][:, perm]
+                snr = self.compute_permuted_snrs_and_inactive(
+                    permuted_pr_batch, ref_noises_batch, noises_activity_mask,
+                    noises_denom_stabilizer, eps=eps)
+                noises_snr_l.append(snr)
+            all_noises_snrs = torch.cat(noises_snr_l, -1)
+            best_noises_snr, _ = torch.max(
+                all_noises_snrs.sum(-2) * num_active_noises, -1)
+
+            best_snr = (self.sources_weight * best_sources_snr +
+                        (1. - self.sources_weight) * best_noises_snr)
+        else:
+            best_snr = None
+            raise ValueError('Not yet avaialble')
+
+        if not self.return_individual_results:
+            best_snr = best_snr.mean()
+
+        if self.backward_loss:
+            return -best_snr
+        return best_snr
+
+    def forward(self,
+                pr_batch,
+                ref_sources_batch,
+                ref_noises_batch,
+                input_mixture,
+                eps=1e-9):
+        """!
+        :param pr_batch: Reconstructed wavs: Torch Tensors of size:
+                         batch_size x self.n_sources x length_of_wavs
+        :param ref_sources_batch: Target wavs: Torch Tensors of size:
+                        batch_size x 2 x length_of_wavs
+        :param ref_noises_batch: Target wavs: Torch Tensors of size:
+                        batch_size x 2 x length_of_wavs
+        :param input_mixture: Target wavs: Torch Tensors of size:
+                        batch_size x 1 x length_of_wavs
+        :param eps: Numerical stability constant.
+        """
+        pr_batch = self.normalize_input(pr_batch)
+        ref_sources_batch = self.normalize_input(ref_sources_batch)
+        ref_noises_batch = self.normalize_input(ref_noises_batch)
+        input_mixture = self.normalize_input(input_mixture)
+
+        snr_l = self.compute_snr(
+            pr_batch, ref_sources_batch, ref_noises_batch,
+            input_mixture, eps=eps)
+
+        return snr_l
+
+
+class PermInvariantSNRwithZeroRefs(nn.Module):
+    """!
+    Class for SNR computation between reconstructed signals and
+    target wavs with compensation for zero reference signals."""
+
+    def __init__(self,
+                 zero_mean=False,
+                 n_sources=None,
+                 backward_loss=True,
+                 valid_permutations=None,
+                 inactivity_threshold=-40.,
+                 return_individual_results=False):
+        """
+        Initialization for the results and torch tensors that might
+        be used afterwards
+        """
+        super().__init__()
+        self.perform_zero_mean = zero_mean
+        self.backward_loss = backward_loss
+        if valid_permutations is None:
+            self.permutations = list(itertools.permutations(
+                torch.arange(n_sources)))
+        else:
+            self.permutations = valid_permutations
         self.permutations_tensor = torch.LongTensor(self.permutations)
         self.n_sources = n_sources
         self.inactivity_threshold = inactivity_threshold
