d71910e65add03592aeb8ea73e8c58fb577b0582
Efthymios Tzinis <etzinis2@illinois.edu>
2021-03-30 22:53:21 -0500

    Adding dataset setup for librifsd50 mixit setup loading

--- approx_ensembles/fedenhance/experiments/utils/dataset_setup.py
+++ approx_ensembles/fedenhance/experiments/utils/dataset_setup.py
@@ -22,3 +22,18 @@ def setup(hparams):
         generators[data_split] = data_loader.get_generator(
             batch_size=hparams['batch_size'], num_workers=hparams['n_jobs'])
     return generators
+
+def mixit_setup(hparams):
+    # Create all generators
+    generators = {}
+    for data_split in ['train', 'val', 'test']:
+        data_loader = libri_fsd.Dataset(
+            root_dirpath=LIBRI_FS50K,
+            speaker_ids=[],
+            split=data_split, sample_rate=hparams['fs'],
+            timelength=hparams['audio_timelength'],
+            zero_pad=True, augment=data_split=='train')
+        generators[data_split] = data_loader.get_generator(
+            batch_size=2*hparams['batch_size'],
+            num_workers=hparams['n_jobs'])
+    return generators
