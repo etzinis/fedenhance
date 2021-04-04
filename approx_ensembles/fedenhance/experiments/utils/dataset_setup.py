5da481ce07eab4fbba6ded215e4072d9faa862a3
Efthymios Tzinis <etzinis2@illinois.edu>
2021-04-04 18:03:56 -0500

    Dataset setup specific eval calls for xreating extra noises

--- approx_ensembles/fedenhance/experiments/utils/dataset_setup.py
+++ approx_ensembles/fedenhance/experiments/utils/dataset_setup.py
@@ -45,14 +45,18 @@ def enhancement_single_node_setup(hparams):
     # Create all generators
     generators = {}
     for data_split in ['train', 'val', 'test']:
+        if data_split == 'train':
+            available_speech_percentage = hparams['available_speech_percentage']
+        else:
+            available_speech_percentage = 0.5
         data_loader = chunked_libri_fsd.Dataset(
             root_dirpath=CHUNK_LIBRI_FS50K,
             speaker_ids=[],
-            available_speech_percentage=hparams['available_speech_percentage'],
+            available_speech_percentage=available_speech_percentage,
             split=data_split, sample_rate=hparams['fs'],
             timelength=hparams['audio_timelength'],
             zero_pad=True, augment=data_split=='train')
         generators[data_split] = data_loader.get_generator(
-            batch_size=2*hparams['batch_size'],
+            batch_size=hparams['batch_size'],
             num_workers=hparams['n_jobs'])
     return generators
