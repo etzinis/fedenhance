0a259f2b0fc8c884cc5559386dd8ad27f0fb2459
Efthymios Tzinis <etzinis2@illinois.edu>
2021-04-04 23:14:20 -0500

    Adding federated enhancement experiments log dir

--- approx_ensembles/fedenhance/__config__.py
+++ approx_ensembles/fedenhance/__config__.py
@@ -2,5 +2,8 @@
 LIBRI_FS50K='/mnt/data/FedEnhance/'
 CHUNK_LIBRI_FS50K='/mnt/data/ChunkFedEnhance/'
 
+# Federated learning experiments log dir
+FED_LOG_DIR='/home/thymios/fedenhance_log/'
+
 # Cometml your key
-API_KEY = 'place_your_cometml_key_here'
+API_KEY = 'place_your_key_here'
