6573c4b19a0fb87c6946e078f513ae49a5446e45
Efthymios Tzinis <etzinis2@illinois.edu>
2021-04-14 16:20:25 -0500

    pretrainign dataset on wham adding path

--- approx_ensembles/fedenhance/__config__.py
+++ approx_ensembles/fedenhance/__config__.py
@@ -1,6 +1,7 @@
 # Datasets paths
 LIBRI_FS50K='/mnt/data/FedEnhance/'
 CHUNK_LIBRI_FS50K='/mnt/data/ChunkFedEnhance/'
+CHUNK_WHAM_ENH='/mnt/data/PreFedEnhance/'
 
 # Federated learning experiments log dir
 FED_LOG_DIR='/home/thymios/fedenhance_log/'
